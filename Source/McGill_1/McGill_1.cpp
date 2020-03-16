2bdb10c2adf8e224b8b736c6cb578e7041bd8026
Lancer <gjtqiyue@gmail.com>
2020-03-16 15:29:32 -0400

    delete everything

--- Source/McGill_1/McGill_1.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "McGill_1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, McGill_1, "McGill_1" );
